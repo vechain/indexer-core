0718f83f60e6bcaf0393c8b21b30549856773108
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-09-12 22:40:05 +0200

    Fix maven URIs and add credentials section

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,12 @@ publishing {
 
     repositories {
         maven {
-            val releasesRepoUrl = uri(layout.buildDirectory.dir("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
-            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            credentials {
+                username = ""
+                password = ""
+            }
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         }
     }
