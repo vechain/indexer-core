649435afb3eeddd0f3c1d6b9c2456211fbab1b3f
nawfal-labrahmi <nawfal.labrahmi@gmail.com>
2023-12-04 11:56:43 +0100

    Fix gpg signing on release

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,8 +70,8 @@ publishing {
     repositories {
         maven {
             credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
+                username = findProperty("ossrhUsername") as String?
+                password = findProperty("ossrhPassword") as String?
             }
             val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
