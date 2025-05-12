30bb36f1c1263d46a1a8cbe9870081b1e16ee84d
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2025-05-12 12:26:03 +0200

    Updating transaction model for galactica hard fork (#34)
    * Updating transaction model for galactica hard fork
    
    * maxFeePerGas and maxPriorityFeePerGas are stored as hex rather than numbers
    
    * Adding a test with dynamic fee transaction
    
    * Adding type field to transaction model and baseFeePerGas field to block model

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "org.vechain"
 
-val projectVersion = System.getenv("PROJECT_VERSION") ?: "3.0.1"
+val projectVersion = System.getenv("PROJECT_VERSION") ?: "4.0.0"
 version = projectVersion
 
 repositories {
