389c984d565164eb7b2530c1477e26bfa18bd240
Daithi Hearn <daithi.hearn@vechain.org>
2025-06-19 18:00:42 +0200

    Updating version to 4.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "org.vechain"
 
-val projectVersion = System.getenv("PROJECT_VERSION") ?: "4.3.0"
+val projectVersion = System.getenv("PROJECT_VERSION") ?: "4.3.1"
 version = projectVersion
 
 repositories {
