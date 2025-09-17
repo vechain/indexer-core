c1ae424d8bef04133d999827bbc2a2fb9db82987
Daithi Hearn <daithi.hearn@vechain.org>
2025-09-17 14:47:49 +0200

    Adding more logging around restarts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 group = "org.vechain"
 
-val projectVersion = System.getenv("PROJECT_VERSION") ?: "5.0.2"
+val projectVersion = System.getenv("PROJECT_VERSION") ?: "5.0.3"
 version = projectVersion
 
 val isSnapshot = version.toString().endsWith("SNAPSHOT")
