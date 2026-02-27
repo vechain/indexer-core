e2a52132d99897f5503120704050eac15a2aa2f2
Daithi Hearn <daithi.hearn@vechain.org>
2026-02-27 10:31:43 +0100

    Update project version to 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ jacoco {
 
 group = "org.vechain"
 
-val projectVersion = System.getenv("PROJECT_VERSION") ?: "7.1.5"
+val projectVersion = System.getenv("PROJECT_VERSION") ?: "7.2.0"
 version = projectVersion
 
 val isSnapshot = version.toString().endsWith("SNAPSHOT")
