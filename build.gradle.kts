8cd32c951a499be1dee18da027c0ff764933a4f9
Daithi Hearn <37153003+daithihearn@users.noreply.github.com>
2026-05-13 12:20:46 +0200

    Hardening unit test (#132)
    * Enhance unit tests for clause deduplication and mapping
    
    * Remove unnecessary test log events from build configuration
    
    * Bump indexer-core version to 10.1.3 in build configuration and README

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ jacoco {
 
 group = "org.vechain"
 
-val projectVersion = System.getenv("PROJECT_VERSION") ?: "10.1.2"
+val projectVersion = System.getenv("PROJECT_VERSION") ?: "10.1.3"
 version = projectVersion
 
 val isSnapshot = version.toString().endsWith("SNAPSHOT")
@@ -125,8 +125,6 @@ tasks.withType<Test> {
             TestLogEvent.FAILED,
             TestLogEvent.PASSED,
             TestLogEvent.SKIPPED,
-            TestLogEvent.STANDARD_ERROR,
-            TestLogEvent.STANDARD_OUT,
         )
         exceptionFormat = TestExceptionFormat.FULL
         showCauses = true
